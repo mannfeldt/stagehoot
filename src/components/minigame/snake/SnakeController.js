0c483b92d35dd2667e2dea03c523524b7ab85e48
emilmannfeldt <emil.mannfeldt@gmail.com>
2021-10-07 23:02:54 +0200

    fix

--- src/components/minigame/snake/SnakeController.js
+++ src/components/minigame/snake/SnakeController.js
@@ -1,12 +1,12 @@
-import React, { Component, Fragment } from 'react';
-import LeftIcon from '@material-ui/icons/ChevronLeft';
-import RightIcon from '@material-ui/icons/ChevronRight';
-import UpIcon from '@material-ui/icons/ExpandLess';
-import DownIcon from '@material-ui/icons/ExpandMore';
-import { Typography } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import PropTypes from 'prop-types';
-import { fire } from '../../../base';
+import React, { Component, Fragment } from "react";
+import LeftIcon from "@material-ui/icons/ChevronLeft";
+import RightIcon from "@material-ui/icons/ChevronRight";
+import UpIcon from "@material-ui/icons/ExpandLess";
+import DownIcon from "@material-ui/icons/ExpandMore";
+import { Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
+import { fire } from "../../../base";
 
 class SnakeController extends Component {
   constructor(props) {
@@ -18,11 +18,14 @@ class SnakeController extends Component {
     const { playerKey, game } = this.props;
     const currentPlayer = game.players[playerKey];
 
-    fire.database().ref(`/games/${game.key}/minigame/snakes/${currentPlayer.snakeId}/move`).set(name, (error) => {
-      if (error) {
-        console.log('error updated snake move');
-      }
-    });
+    fire
+      .database()
+      .ref(`/games/${game.key}/minigame/snakes/${currentPlayer.snakeId}/move`)
+      .set(name, (error) => {
+        if (error) {
+          console.log("error updated snake move");
+        }
+      });
   }
 
   render() {
@@ -31,37 +34,70 @@ class SnakeController extends Component {
     const playerSnake = game.minigame.snakes[currentPlayer.snakeId];
 
     const allControlActions = playerSnake.actions;
-    const displayName = playerSnake.playerKeys.length === 1 ? currentPlayer.name : playerSnake.name;
+    const displayName =
+      playerSnake.playerKeys.length === 1
+        ? currentPlayer.name
+        : playerSnake.name;
 
     return (
-      <div className="phase-container">
-        <div className="player-controlls-container">
-          {allControlActions.map(action => (
-            <Fragment key={action}>
-              {currentPlayer.controlActions.includes(action)
-                ? (
-                  <Button key={action} className={`player-controlls-action player-controlls-${action}`} onClick={() => { this.handleAction(action); }} style={{ backgroundColor: playerSnake.color }}>
-                    {action === 'up' && <UpIcon className="player-controlls-icon" />}
-                    {action === 'down' && <DownIcon className="player-controlls-icon" />}
-                    {action === 'right' && <RightIcon className="player-controlls-icon" />}
-                    {action === 'left' && <LeftIcon className="player-controlls-icon" />}
-                  </Button>
-                )
-                : (
-                  <Button key={action} className={`player-controlls-action player-controlls-${action}`} style={{ backgroundColor: playerSnake.color, opacity: 0.3 }}>
-                    {action === 'up' && <UpIcon className="player-controlls-icon" />}
-                    {action === 'down' && <DownIcon className="player-controlls-icon" />}
-                    {action === 'right' && <RightIcon className="player-controlls-icon" />}
-                    {action === 'left' && <LeftIcon className="player-controlls-icon" />}
-                  </Button>
-                )
-                            }
-            </Fragment>
-          ))}
-          <div className="player-controlls-action player-controlls-middle">
-            <Typography variant="body1">{displayName}</Typography>
-          </div>
-        </div>
+      <div className="snake-controller-container">
+        <div style={{ position: "absolute", zIndex: 100 }}>{displayName}</div>
+        <Button
+          key={"up"}
+          className="snake-controller-button-fullwidth"
+          onClick={() => {
+            this.handleAction("up");
+          }}
+          disabled={!currentPlayer.controlActions.includes("up")}
+          style={{
+            backgroundColor: playerSnake.color,
+            opacity: currentPlayer.controlActions.includes("up") ? 1.0 : 0.3,
+          }}
+        >
+          <UpIcon className="player-controlls-icon" />
+        </Button>
+        <Button
+          key={"left"}
+          className="snake-controller-button"
+          onClick={() => {
+            this.handleAction("left");
+          }}
+          disabled={!currentPlayer.controlActions.includes("left")}
+          style={{
+            backgroundColor: playerSnake.color,
+            opacity: currentPlayer.controlActions.includes("left") ? 1.0 : 0.3,
+          }}
+        >
+          <LeftIcon className="player-controlls-icon" />
+        </Button>
+        <Button
+          key={"right"}
+          className="snake-controller-button"
+          disabled={!currentPlayer.controlActions.includes("right")}
+          onClick={() => {
+            this.handleAction("right");
+          }}
+          style={{
+            backgroundColor: playerSnake.color,
+            opacity: currentPlayer.controlActions.includes("right") ? 1.0 : 0.3,
+          }}
+        >
+          <RightIcon className="player-controlls-icon" />
+        </Button>
+        <Button
+          key={"down"}
+          className="snake-controller-button-fullwidth"
+          disabled={!currentPlayer.controlActions.includes("down")}
+          onClick={() => {
+            this.handleAction("down");
+          }}
+          style={{
+            backgroundColor: playerSnake.color,
+            opacity: currentPlayer.controlActions.includes("down") ? 1.0 : 0.3,
+          }}
+        >
+          <DownIcon className="player-controlls-icon" />
+        </Button>
       </div>
     );
   }
